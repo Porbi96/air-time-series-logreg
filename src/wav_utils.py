a2de2f6c292e4ad0d494afd89b38e77784820a7f
Jakub Poręba <spikasny@gmail.com>
2022-04-09 13:00:30 +0200

    Add multiclass LR implementation, clean up util functions, add jupyter notebook demo, update gitignore

--- src/wav_utils.py
+++ src/wav_utils.py
@@ -1,8 +1,11 @@
 import os
 from typing import List
 
+import numpy as np
 import skfda
 from scipy.io import wavfile
+from scipy.fft import fft, fftfreq
+
 
 def create_wav_matrix_raw(directory: str) -> List[List[int]]:
     if not os.path.isdir(directory):
@@ -16,7 +19,7 @@ def create_wav_matrix_raw(directory: str) -> List[List[int]]:
     return data_matrix
 
 
-def create_wav_fdatagrid(directory):
+def create_wav_fdatagrid(directory: str):
     if not os.path.isdir(directory):
         raise NotADirectoryError('Directory not found')
 
@@ -35,4 +38,21 @@ def create_wav_fdatagrid(directory):
     return skfda.FDataGrid(
         data_matrix=data_matrix,
         grid_points=grid_points,
+    )
+
+
+def transform_time_to_fft(fDataGrid, normalize=True):
+    fft_data_matrix = []
+    samplerate = len(fDataGrid.grid_points[0])
+
+    for row in fDataGrid.data_matrix:
+        data = 2.0 / samplerate * np.abs(fft(row)[0:samplerate // 2])
+        if normalize:
+            data = (data - np.min(data)) / (np.max(data) - np.min(data))
+        fft_data_matrix.append(data)
+
+    xf = fftfreq(samplerate, 1 / samplerate)[:samplerate // 2]
+    return skfda.FDataGrid(
+        data_matrix=fft_data_matrix,
+        grid_points=xf,
     )
